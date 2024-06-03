20b03f4bfec427fb8df6e86dcc81f4aefd05c988
MohiZzine <farid.mohieddine3@gmail.com>
2024-06-03 04:44:13 +0100

    MIScikit-Learn 0.1.0 PACKAGED SUCCESSFULLY

--- setup.py
+++ setup.py
@@ -1,32 +1,30 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Mini Scikit-Learn Library',
-    version='0.1.0',
-    author='Mohieddine Farid | Image Fjer',
-    author_email='farid.mohieddine@um6p.ma',
-    description='A lightweight machine-learning library inspired by Scikit-Learn',
+    name="MIScikit-Learn",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+        "scipy",
+        "pandas",
+        "matplotlib",
+    ],
+    entry_points={
+        "console_scripts": [
+        ],
+    },
+    author="Mohieddine Farid | Imane Fjer",
+    author_email="farid.mohieddine@um6p.ma | imane.fjer@um6p.ma",
+    description="A lightweight machine learning library inspired by scikit-learn.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/MohiZzine/mini-scikit-learn',
-    packages=find_packages(),
-    install_requires=[
-        'numpy>=1.18.5',
-        'pandas>=1.0.5',
-        'scikit-learn>=0.23.1' 
-    ],
+    url="https://github.com/MohiZzine/mini-scikit-learn",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
 )
-
